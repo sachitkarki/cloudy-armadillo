4e258cdff72cc055d6686b61e4b10e0368942ce4
robert.lyndon.davis <robert.lyndon.davis@bf1e086f-736c-fcba-26d7-d6ae2471024b>
2011-03-23 12:43:47 +0000

    started work on the intent registration for the c2dm functionality testing

--- Cloudy Armadillo/src/com/monkelabs/cloudyarmadillo/Main.java
+++ Cloudy Armadillo/src/com/monkelabs/cloudyarmadillo/Main.java
@@ -1,7 +1,9 @@
 package com.monkelabs.cloudyarmadillo;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.os.Bundle;
+import android.content.*;
 
 public class Main extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,21 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        
+        
+    }
+    
+    private void RegisterIntent()
+    {
+    	Intent regIntent = new Intent(getString(R.string.c2dm_google_register_path));
+    	
+    	regIntent.putExtra("app", 
+    			PendingIntent.getBroadcast(this, 0, new Intent(), 0));
+    	
+    	
+    			
+    	
     }
 }
